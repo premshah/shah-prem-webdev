d62338c167d94cdb3985d845425ac9a99a81aa32
Prem Shah <prem@prem-Inspiron-3520>
2017-03-22 23:25:34 -0400

    assignment 5 v2

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,6 +1,17 @@
 module.exports = function () {
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/web-spring-2017');
+
+    var connectionString = 'mongodb://localhost/web-spring-2017';
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+    mongoose.connect(connectionString);
+
 
     var UserModel = require("./user/user.model.server")();
     var PageModel = require("./page/page.model.server")();
